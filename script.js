a22255f08aed3419e5d94b088ad052d5ccdbe69a
fareeza05 <fareezarahman123@gmail.com>
2023-07-18 21:33:47 -0400

    script.js updated - 3 more features left to add

--- script.js
+++ script.js
@@ -8,3 +8,42 @@ let firstOperand = getElementById('firstOperand')
 let secondOperand = getElementById('secondOperand')
 
 
+class Calculator {
+
+    constructor(firstInput, secondInput){
+        this.firstInput = firstInput;
+        this.secondInput = secondInput;
+        this.clear();
+    }
+
+    clear(){
+        // sets first number, second number and operator to blank strings and undefined value
+        this.firstInput = ''
+        this.secondInput = ''
+        this.operator = undefined;
+    }
+
+    delete(){
+        //example of string slicing in JS. deletes the last element.
+        // 0 = starting index, -1 = begins at the end of string, slice = gets rid of the char
+        this.secondInput = this.secondInput.toString().slice(0, -1);
+    }
+
+    addDecimal(decimal){
+
+        if(this.secondInput = ''){
+            this.secondInput = this.secondInput.toString() + '0' + decimal.toString();
+        } else {
+            this.secondInput = this.secondInput.toString() + decimal.toString();
+        }
+
+
+
+        if(decimal !== '.' || this.secondInput.includes('.')) {
+            return; //exit method early
+        } else {
+        this.secondInput = this.secondInput.toString() + decimal.toString();
+        }
+    }
+
+}
